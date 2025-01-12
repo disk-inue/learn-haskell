31d0c4e64393fffb66ec803ece0455925c4a0dfa
disk-inue <disk.inue.01@gmail.com>
2025-01-12 13:42:48 +0900

    feat : prompt関数追加

--- src/calculator.hs
+++ src/calculator.hs
@@ -1,10 +1,15 @@
+import System.IO (hFlush, stdout)
+
+prompt :: String -> IO String
+prompt message = do
+  putStr message
+  hFlush stdout
+  getLine
+
 main = do
   putStrLn "start calculator"
   putStrLn "q is end calculator"
-  putStrLn "number > "
-  inputLeftNumber <- getLine
-  putStrLn "four arithmetic operations(+, -, *, /) > "
-  inputArithmetic <- getLine
-  putStrLn "number > "
-  inputRightNumber <- getLine
+  inputLeftNumber <- prompt "number > "
+  inputArithmetic <- prompt "four arithmetic operations(+, -, *, /) > "
+  inputRightNumber <- prompt "number > "
   putStrLn (inputLeftNumber ++ " " ++ inputArithmetic ++ " " ++ inputRightNumber ++ " = ")
