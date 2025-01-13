22446258065a84d814ee0f5dd1eca06734cd8124
disk-inue <disk.inue.01@gmail.com>
2025-01-13 16:28:52 +0900

    feat : calculate result

--- src/calculator.hs
+++ src/calculator.hs
@@ -6,10 +6,25 @@ prompt message = do
   hFlush stdout
   getLine
 
+calculate :: Double -> String -> Double -> Double
+calculate leftNumber arithmetic rightNumber = do
+  case arithmetic of
+    "+" -> leftNumber + rightNumber
+    "-" -> leftNumber - rightNumber
+    "*" -> leftNumber * rightNumber
+    "/" -> leftNumber / rightNumber
+
 main = do
   putStrLn "start calculator"
   putStrLn "q is end calculator"
+
   inputLeftNumber <- prompt "number > "
+  let leftNumber = read inputLeftNumber :: Double
+
   inputArithmetic <- prompt "four arithmetic operations(+, -, *, /) > "
+
   inputRightNumber <- prompt "number > "
-  putStrLn (inputLeftNumber ++ " " ++ inputArithmetic ++ " " ++ inputRightNumber ++ " = ")
+  let rightNumber = read inputRightNumber :: Double
+
+  let result = calculate leftNumber inputArithmetic rightNumber
+  putStrLn (inputLeftNumber ++ " " ++ inputArithmetic ++ " " ++ inputRightNumber ++ " = " ++ show result)
